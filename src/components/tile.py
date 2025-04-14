f4b47770694e90efd710cc1b0ea193ced745c632
brayden <brayden.riesberg@aol.com>
2025-04-13 22:52:58 -0500

    reformat tile

--- src/components/tile.py
+++ src/components/tile.py
@@ -1,13 +1,13 @@
-from abc import ABC
 import random
+from abc import ABC
 
 import pygame as pg
 
 from paths import *
-from src.config import Config
+from src.common import MENU, TILE_IMAGE_PATHS
 from src.components.popup import Popup
 from src.components.ticket import Ticket
-from src.shared_data import TILE_IMAGE_PATHS, MENU
+from src.config import Config
 
 
 # Only inherited from.
@@ -24,12 +24,14 @@ class Tile(ABC):
         # Will overwrite this rect immediately.
         self.rect = rect
         if not self.containers:
-            raise ValueError('Must define groups for this class.')
+            raise ValueError("Must define groups for this class.")
         self.add(self.containers)
+
 
 class Floor(Tile, pg.sprite.Sprite):
     def __init__(self, kind, rect):
         super().__init__(kind, rect)
+
 
 class Appliance(Tile, pg.sprite.Sprite):
     def __init__(self, kind, rect):
@@ -47,12 +49,12 @@ class Appliance(Tile, pg.sprite.Sprite):
             self._unshow_interact_popup()
 
     def get_hitbox(self) -> pg.Rect:
-        '''Return the area where the user can interact.'''
+        """Return the area where the user can interact."""
         return self.zone
 
     def _show_interact_popup(self):
         self.popup.add(Popup.containers)
-    
+
     def _unshow_interact_popup(self):
         self.popup.kill()
 
@@ -79,24 +81,21 @@ class Table(Appliance, pg.sprite.Sprite):
     def update(self, elapsed, *args, **kwargs):
         self.elapsed = elapsed
 
-
     def _decide_order(self) -> None:
-        '''Choose an item to order and call for the chef.'''
+        """Choose an item to order and call for the chef."""
         # I think I'll have a...
         self.order = random.choice(list(MENU.keys()))
         # Show popup.
         self.popup.add(Popup.containers)
 
     def tell_order(self) -> str:
-        '''Return the randomly decided dish. Unshow popup.'''
+        """Return the randomly decided dish. Unshow popup."""
         self.popup.kill()
-        # Will return 'falsy' if order is not decided. 
-        print(self.order) 
+        # Will return 'falsy' if order is not decided.
+        print(self.order)
         return self.order
-    
+
     def receive_dish(self) -> None:
-        '''Handles logic for a table getting a dish.'''
+        """Handles logic for a table getting a dish."""
         # Remove the sprite from the game.
         self.order = None
-
-
