26432f58a28016248b1d4f2a7cdebc57aac2f5af
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-06-19 16:36:15 -0500

    move files

--- paths.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-ASSET_DIR = Path('assets')
-IMAGE_DIR = ASSET_DIR / 'images'
-AUDIO_DIR = ASSET_DIR / 'audio'
-FONT_DIR = ASSET_DIR / 'fonts'
--- src/gamestates/level.py
+++
@@ -1,4 +0,0 @@
-import pygame as pg
-
-from paths import *
-from src.gamestates.gamestate import GameState
