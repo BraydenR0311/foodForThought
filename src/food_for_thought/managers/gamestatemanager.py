b333d540e57db1b85bd3332d9fb42829271e6220
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-06-22 10:01:04 -0500

    improve structure of gamestate

--- src/food_for_thought/managers/gamestatemanager.py
+++ src/food_for_thought/managers/gamestatemanager.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pygame as pg
 
 from .. import config
@@ -26,6 +28,26 @@ class GameStateManager:
     def quit(self):
         self._running = False
 
+    def goto(
+        self, statekey, data: dict[str, Any] | None = None, teardown=False
+    ) -> None:
+        current_state = self.get_current_state()
+        if current_state is not None:
+            current_state.exit()
+            if teardown:
+                current_state.teardown()
+
+        self._current_state_key = statekey
+        current_state = self.get_current_state()
+
+        if data is not None:
+            self.send_data(data)
+
+        if not current_state.issetup():
+            current_state.setup()
+
+        current_state.enter()
+
     def get_current_state(self):
         if self._current_state_key is not None:
             try:
@@ -36,36 +58,11 @@ class GameStateManager:
                     f" gamestatemanager."
                 )
 
-    def goto(self, statekey, data=None, teardown=False) -> None:
-        if self.get_current_state() is not None:
-            if teardown:
-                self.get_current_state().teardown()
-
-            self.get_current_state().exit()
-
-        self._current_state_key = statekey
-
-        if not self.get_current_state().issetup():
-            self.get_current_state().setup()
-
-        if data is not None:
-            self.get_current_state().update_data(data)
-
-        self.get_current_state().enter()
-
     def run(self):
         self.get_current_state().run()
 
         pg.display.flip()
         self.clock.tick(config.FPS)
 
-    def transfer_data(self, from_statekey, to_statekey):
-        try:
-            from_gamestate = self.gamestates[from_statekey]
-            to_gamestate = self.gamestates[to_statekey]
-        except KeyError:
-            print("Unable to transfer data!")
-            raise
-
-        data = from_gamestate.get_data()
-        to_gamestate.update_data(data)
+    def send_data(self, data: dict[str, Any]) -> None:
+        self.get_current_state().update_data(data)
