b79a43b912b225b7ee2fed4ac6839279c590560e
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-10-26 14:11:43 -0500

    feat: implement sound

--- src/food_for_thought/managers/audiomanager.py
+++ src/food_for_thought/managers/audiomanager.py
@@ -2,19 +2,50 @@ import random
 
 import pygame as pg
 
-from .. import config
+from ..config import SOUND_DIR, MUSIC_DIR
+import random
 
 
 class AudioManager:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self.songs = list((config.AUDIO_DIR / "music").iterdir())
+        if AudioManager._initialized:
+            return
+        pg.mixer.init()
+        AudioManager._initialized = True
+        self.songs = [MUSIC_DIR / f for f in MUSIC_DIR.iterdir()]
+        self.queue = self._get_random_music_queue()
+        self.current_track = None
+        self.sounds = {
+            "sizzle": pg.mixer.Sound(SOUND_DIR / "sizzle.ogg"),
+            "chop": pg.mixer.Sound(SOUND_DIR / "chop.ogg"),
+            "click_up": pg.mixer.Sound(SOUND_DIR / "click_up.opus"),
+            "click_down": pg.mixer.Sound(SOUND_DIR / "click_down.opus"),
+        }
 
     def play_music(self):
-        if not pg.mixer.music.get_busy():
-            song = random.choice(self.songs)
-            pg.mixer.music.load(song)
-            pg.mixer.music.play()
+        if pg.mixer.music.get_busy():
+            return
+        if not self.queue:
+            self.queue = self._get_random_music_queue()
+        self.current_track = self.queue.pop()
+        pg.mixer.music.load(self.current_track)
+        pg.mixer.music.play()
 
-    def play_sound(self, sound_file):
-        sound = pg.mixer.Sound(sound_file)
-        sound.play()
+    def play_sound(self, sound_name):
+        self.sounds[sound_name].play()
+
+    def stop_sound(self, sound_name):
+        self.sounds[sound_name].stop()
+
+    def _get_random_music_queue(self) -> list:
+        queue = self.songs.copy()
+        random.shuffle(queue)
+        return queue
