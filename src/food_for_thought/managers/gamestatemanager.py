716da6f5c412a4044c99a83eb7d00298129e9822
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-10-26 14:11:56 -0500

    fix: convert to singleton

--- src/food_for_thought/managers/gamestatemanager.py
+++ src/food_for_thought/managers/gamestatemanager.py
@@ -1,8 +1,13 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import pygame as pg
 
 from .. import config
+
+if TYPE_CHECKING:
+    from ..gamestates.gamestate import GameState
+
+from ..gamestates.statekey import StateKey
 
 
 class MissingGamestateError(Exception):
@@ -10,17 +15,26 @@ class MissingGamestateError(Exception):
 
 
 class GameStateManager:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self._current_state_key = None
-        self.gamestates = {}
+        if GameStateManager._initialized:
+            return
+        GameStateManager._initialized = True
+        self._current_statekey = StateKey.MAIN_MENU
+        self._gamestates = {}
         self.clock = pg.time.Clock()
         self._running = True
 
-    def register_gamestate(self, gamestate, audiomanager, visualmanager):
-        self.gamestates[gamestate.get_statekey()] = gamestate
-        gamestate.set_gsmanager(self)
-        gamestate.set_audiomanager(audiomanager)
-        gamestate.set_visualmanager(visualmanager)
+    def register_gamestate(self, *gamestates: "GameState"):
+        for gs in gamestates:
+            self._gamestates[gs.get_statekey()] = gs
 
     def is_running(self):
         return self._running
@@ -32,37 +46,34 @@ class GameStateManager:
         self, statekey, data: dict[str, Any] | None = None, teardown=False
     ) -> None:
         current_state = self.get_current_state()
-        if current_state is not None:
-            current_state.exit()
-            if teardown:
-                current_state.teardown()
+        current_state.exit(teardown)
 
-        self._current_state_key = statekey
-        current_state = self.get_current_state()
+        self._current_statekey = statekey
+        new_state = self.get_current_state()
 
         if data is not None:
             self.send_data(data)
 
-        if not current_state.issetup():
-            current_state.setup()
+        new_state.enter()
 
-        current_state.enter()
-
-    def get_current_state(self):
-        if self._current_state_key is not None:
-            try:
-                return self.gamestates[self._current_state_key]
-            except KeyError:
-                raise MissingGamestateError(
-                    f"{self._current_state_key.name} statekey not registered with"
-                    f" gamestatemanager."
-                )
+    def get_current_state(self) -> "GameState":
+        return self._gamestates[self._current_statekey]
 
     def run(self):
+        dt = self.clock.tick(config.FPS) / 1000
+        events = pg.event.get()
+
+        for event in events:
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                self.quit()
+
+        # Make sure current state has a copy of the current events.
+        self.send_data({"events": events, "dt": dt})
         self.get_current_state().run()
 
         pg.display.flip()
-        self.clock.tick(config.FPS)
 
     def send_data(self, data: dict[str, Any]) -> None:
         self.get_current_state().update_data(data)
