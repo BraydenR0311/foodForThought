d03fe1c2ffb8992f98ca7b945ef0626d80ebc383
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-10-26 14:11:08 -0500

    fix: gamestate logic

--- src/food_for_thought/gamestates/gamestate.py
+++ src/food_for_thought/gamestates/gamestate.py
@@ -1,26 +1,11 @@
 from abc import ABC, abstractmethod
-from ..managers.gamestatemanager import GameStateManager
-from ..managers.visualmanager import VisualManager
-from ..managers.audiomanager import AudioManager
 
 
 class GameState(ABC):
     def __init__(self, statekey):
-        self._gsmanager: None | GameStateManager = None
-        self._visualmanager: None | VisualManager = None
-        self._audiomanager: None | AudioManager = None
         self.data = {}
         self._statekey = statekey
-        self._issetup = False
-
-    def set_gsmanager(self, gsmanager: GameStateManager):
-        self._gsmanager = gsmanager
-
-    def set_visualmanager(self, visualmanager: VisualManager):
-        self._visualmanager = visualmanager
-
-    def set_audiomanager(self, audiomanager: AudioManager):
-        self._audiomanager = audiomanager
+        self._is_setup = False
 
     def get_statekey(self):
         return self._statekey
@@ -28,25 +13,18 @@ class GameState(ABC):
     def update_data(self, new_data):
         self.data.update(new_data)
 
-    def play_sound(self, sound):
-        self._audiomanager.play_sound(sound)
-
-    def issetup(self):
-        return self._issetup
-
-    def setup(self):
-        self._issetup = True
-        self._setup()
-
-    def teardown(self):
-        self._issetup = False
-        self._teardown()
-
     def enter(self, *args, **kwargs):
-        pass
+        if not self._is_setup:
+            self._is_setup = True
+            self._setup()
+        self._enter()
 
-    def exit(self, *args, **kwargs):
-        pass
+    def exit(self, teardown=False, *args, **kwargs):
+        self._exit()
+        if teardown:
+            self._is_setup = False
+
+            self._teardown()
 
     @abstractmethod
     def run(self, *args, **kwargs):
@@ -56,8 +34,14 @@ class GameState(ABC):
     def _setup(self, *args, **kwargs):
         pass
 
+    def _enter(self, *args, **kwargs):
+        pass
+
     @abstractmethod
     def _teardown(self, *args, **kwargs):
+        pass
+
+    def _exit(self, *args, **kwargs):
         pass
 
     @abstractmethod
