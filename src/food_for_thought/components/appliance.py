a126b180b022dd7339ace4816f514df022d73ad9
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-11-05 21:53:54 -0600

    fix: breakout appliance in separate module

---
+++ src/food_for_thought/components/appliance.py
@@ -0,0 +1,24 @@
+import pygame as pg
+
+from .tile import InteractTile
+from ..gamestates.statekey import StateKey
+import logging
+from .. import game_events
+from enum import Enum, auto
+
+logger = logging.getLogger(__name__)
+
+
+class Appliance(InteractTile):
+    def __init__(
+        self,
+        type: str,
+        rect: pg.Rect,
+    ):
+        super().__init__(type, rect)
+
+    def interact(self, player):
+        ticket = player.get_ticket()
+        if not ticket:
+            return
+        pg.event.post(pg.event.Event(game_events.APPLIANCE_COOK, {"appliance": self}))
