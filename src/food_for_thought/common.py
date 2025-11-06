8bb9f6194046dde1c55c4a0e2416798b4d83385c
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-11-05 21:52:16 -0600

    move stuff out of common

--- src/food_for_thought/common.py
+++ src/food_for_thought/common.py
@@ -1,24 +1,5 @@
 from . import config
 from .utils.utils import get_quotes
+from enum import Enum, auto
 
 QUOTE_DATA = get_quotes(config.ASSET_DIR / "quotes.json")
-
-TILE_IMAGE_PATHS = {
-    "#": config.TILE_DIR / "floor.png",
-    "x": config.TILE_DIR / "floor.png",
-    "f": config.TILE_DIR / "fryer.png",
-    "o": config.TILE_DIR / "oven.png",
-    "c": config.TILE_DIR / "cutting.png",
-    "t": config.TILE_DIR / "table.png",
-}
-
-APPLIANCE_DICT = {
-    "cheese": "c",
-    "patty": "o",
-    "bun": "o",
-    "beef": "o",
-    "tomato": "c",
-    "shell": "o",
-}
-
-MENU = {"burger": ["bun", "patty", "cheese"], "taco": ["shell", "beef", "tomato"]}
