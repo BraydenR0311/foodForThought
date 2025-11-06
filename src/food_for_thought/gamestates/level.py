5c321c1739a2629bc7733afa903300bdb00dc1f6
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-11-05 21:59:15 -0600

    fix: update level

--- src/food_for_thought/gamestates/level.py
+++ src/food_for_thought/gamestates/level.py
@@ -4,23 +4,90 @@ import pygame as pg
 
 from .. import config
 from .. import groups
-from ..utils.utils import read_tilemap
 from .gamestate import GameState
 from .statekey import StateKey
 
 from ..components.player import Player
-from ..components.tile import Floor, Appliance
+from ..components.tile import Floor, TileType
+from ..components.appliance import Appliance
+
 from ..components.table import Table
 from ..components.levelclock import LevelClock
 from ..managers.gamestatemanager import GameStateManager
 from ..managers.visualmanager import VisualManager
 from ..managers.audiomanager import AudioManager
 from ..managers.tablemanager import TableManager
+from .. import game_events
+from pathlib import Path
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 gamestate_manager = GameStateManager()
 visual_manager = VisualManager()
 audio_manager = AudioManager()
 table_manager = TableManager()
+
+
+def read_tilemap(fp: Path) -> pg.Rect:
+    with open(fp, "r", encoding="utf-8") as infile:
+        tilemap = infile.read().splitlines()
+
+    gridwidth = len(tilemap[0])
+    if gridwidth % 2 == 0:  # Grid has even number of tiles.
+        topleftx = visual_manager.get_screen_rect().centerx - (
+            gridwidth // 2 * config.TILESIZE
+        )
+        toplefty = (
+            visual_manager.get_screen_rect().centery
+            - (gridwidth // 2 * config.TILESIZE),
+        )
+    else:  # Grid has odd number of tiles.
+        topleftx = (
+            visual_manager.get_screen_rect().centerx
+            - (gridwidth // 2 * config.TILESIZE)
+            - (config.TILESIZE // 2)
+        )
+        toplefty = (
+            visual_manager.get_screen_rect().centery
+            - (gridwidth // 2 * config.TILESIZE)
+            - (config.TILESIZE // 2)
+        )
+
+    for i, row in enumerate(tilemap):
+        for j, tile in enumerate(row):
+            rect = pg.Rect(
+                topleftx + j * config.TILESIZE,
+                toplefty + i * config.TILESIZE,
+                config.TILESIZE,
+                config.TILESIZE,
+            )
+
+            match tile:
+                case TileType.floor.value:
+                    Floor(TileType.floor, rect)
+                case TileType.player.value:
+                    Floor(TileType.floor, rect)
+                    Player(rect.center)
+                case TileType.table.value:
+                    Table(TileType.table, rect)
+                case TileType.oven.value:
+                    Appliance(TileType.oven, rect)
+                case TileType.cutting_board.value:
+                    Appliance(TileType.cutting_board, rect)
+                case TileType.fryer.value:
+                    Appliance(TileType.fryer, rect)
+                case _:
+                    logger.error("Unknown TileType: '%s'", tile)
+
+    kitchen_rect = pg.Rect(
+        topleftx, toplefty, gridwidth * config.TILESIZE, gridwidth * config.TILESIZE
+    )
+
+    return kitchen_rect
 
 
 class Level(GameState):
@@ -31,10 +98,6 @@ class Level(GameState):
     def _setup(self):
         self.kitchen_rect = read_tilemap(
             config.ASSET_DIR / "map.txt",
-            Player,
-            Floor,
-            Appliance,
-            Table,
         )
         self.level_clock = LevelClock()
         self.player: Player = groups.player_group.sprite
@@ -56,6 +119,11 @@ class Level(GameState):
         for event in events:
             if event.type == pg.KEYDOWN and event.key == pg.K_e:
                 e_press = True
+            elif event.type == game_events.TABLE_RECEIVE_DISH:
+                pass
+            elif event.type == game_events.APPLIANCE_COOK:
+                logger.debug("Cooking on %s", event.appliance)
+                gamestate_manager.goto(StateKey.COOK)
 
         keys = pg.key.get_pressed()
         player = self.player
@@ -111,9 +179,8 @@ class Level(GameState):
 
     @override
     def _update(self):
-        groups.all_sprites.update(
-            elapsed=self.level_clock.get_elapsed(), dt=self.data["dt"]
-        )
+        groups.all_sprites.update(self.level_clock.get_elapsed(), self.data["dt"])
+        table_manager.update(self.level_clock.get_elapsed())
 
     @override
     def _draw(self):
