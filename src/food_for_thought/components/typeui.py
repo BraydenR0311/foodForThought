22d3b650581c31ead5920531f072f1a5aca2fff2
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-11-30 13:59:21 -0600

    fix: improve type ui

--- src/food_for_thought/components/typeui.py
+++ src/food_for_thought/components/typeui.py
@@ -10,18 +10,29 @@ visual_manager = VisualManager()
 
 
 class TypeUI:
-    def __init__(self, quote_part: str, current_tick: int, fontsize: int) -> None:
-        self._text = Text(quote_part, fontsize, "black")
+    def __init__(self, quote_chunk: str, level_elapsed: int, fontsize: int) -> None:
+        self._text = Text(
+            quote_chunk,
+            fontsize,
+            "black",
+            midbottom=visual_manager.get_screen_rect().move(0, -20).midbottom,
+        )
         # Time is based on number of words and then some.
         self._timer = TypeTimer(
-            len(self._text.get_content().split()) + 3, current_tick, 15, "black"
+            len(self._text.get_content().split()) + 3, level_elapsed, 15, "black"
         )
         # User's typed text that will mask original text.
-        self._user_input = Text("", fontsize, "green")
+        self._user_input = Text(
+            "",
+            fontsize,
+            "green",
+            topleft=self._text.rect.topleft,
+        )
+        self._user_input_buffer = ""
         self._is_erring = False
         self._misses = 0
 
-    def is_time_up(self) -> bool:
+    def times_up(self) -> bool:
         """Timer is done."""
         return self._timer.is_done()
 
@@ -29,7 +40,7 @@ class TypeUI:
         """User has successfully written text."""
         return self._text.get_content() == self._user_input.get_content()
 
-    def get_misses(self) -> int:
+    def num_misses(self) -> int:
         """Return number of misses"""
         return self._misses
 
@@ -42,36 +53,35 @@ class TypeUI:
     def update(self, elapsed) -> None:
         """Need elapsed time from an object that manages level time."""
         self._timer.update(elapsed)
+        self._text.update()
+        self._user_input.update()
 
-    def handle_input(self, events: list[pg.Event]) -> None:
+    def handle_input(self, events: list[pg.event.Event]) -> None:
         """Handle input from list of events from pg.event.get()"""
         # Enforce whether or not user input is matching text.
-        self._handle_erring()
+        # self._handle_erring()
 
         for event in events:
             # User types something.
             if event.type == pg.TEXTINPUT:
-                if event.text.isascii() and not self._is_erring:
-                    # Add new character.
-                    self._user_input.add_char(event.text)
+                if not event.text.isascii():
+                    continue
+                if not self._is_erring:
+                    self._user_input_buffer += event.text
+                    if not self._text.get_content().startswith(self._user_input_buffer):
+                        self._is_erring = True
+                        self._text.change_color("red")
+                        self._user_input.change_color("red")
+                        self._misses += 1
+                        self._timer.add_wrong()
+                    else:
+                        self._user_input.replace_text(self._user_input_buffer)
+
             # User presses backspace.
             elif event.type == pg.KEYDOWN and event.key == pg.K_BACKSPACE:
                 # Remove a character.
-                self._user_input.remove_char()
-
-    def _handle_erring(self) -> None:
-        """Update status of whether or not user input is wrong."""
-        # User input does not line up with text.
-        if not self._text.get_content().startswith(self._user_input.get_content()):
-            # If previously not erring...
-            if not self._is_erring:
-                self._is_erring = True
-                self._misses += 1
-                self._text.change_color("red")
-                # Display red check under timer.
-                self._timer.add_wrong()
-        else:
-            # If previously erring...
-            if self._is_erring:
-                self._is_erring = False
-                self._text.change_color("green")
+                if self._is_erring:
+                    self._is_erring = False
+                    self._user_input_buffer = self._user_input_buffer[:-1]
+                    self._user_input.change_color("green")
+                    self._text.change_color("black")
