f1b73792d2472cdc256d4d9c429b7e52530b262d
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-11-30 13:58:04 -0600

    fix: improve ticket init

--- src/food_for_thought/components/ticket.py
+++ src/food_for_thought/components/ticket.py
@@ -42,30 +42,37 @@ class Ticket(pg.sprite.Sprite):
     containers = None
     images = {}
 
-    def __init__(self, dish_name):
+    def __init__(self, dish_name: str):
         super().__init__(self.containers)
         self._dish_name = dish_name
         self._quote = Quote()
-        self._ingredients = MENU[self._dish_name].ingredients
+        self._grid = [[(30, 30), (60, 30)], [(30, 60), (60, 60)], [(30, 90), (60, 90)]]
+        self._ingredients = [
+            TicketIngredient(
+                ingredient,
+                food_coordinate=self._grid[i][0],
+                status_coordinate=self._grid[i][1],
+            )
+            for i, ingredient in enumerate(MENU[self._dish_name].ingredients)
+        ]
         self._num_wrong = 0
         self._num_correct = 0
         # Topleft of each sprite.
-        self._grid = [[(30, 30), (60, 30)], [(30, 60), (60, 60)], [(30, 90), (60, 90)]]
         self._sprites = pg.sprite.Group()
 
         self.image = self.images["ticket"]
 
         # Position sprite.
         self.rect = self.image.get_rect(midtop=visual_manager.get_screen_rect().midtop)
-        self.rect.move_ip(10, 0)
+        self.rect.move_ip(0, 10)
 
         # Display food.
-        for ingredient, coordinate in zip(self._ingredients, self._grid):
+        for ingredient in self._ingredients:
             self._sprites.add(
                 Generic(
-                    ingredient.image_path,
+                    ingredient.metadata.image_path,
                     (Ticket.IMAGE_SIZE, Ticket.IMAGE_SIZE),
-                    topleft=self.rect.move(coordinate[0]).topleft,
+                    topleft=self.rect.move(ingredient.food_coordinate).topleft,
                 )
             )
 
